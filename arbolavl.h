62da2c499ed607de953ab1f062b812edf1fe0a60
Eduardo <EduardoGarcia360@users.noreply.github.com>
2016-10-28 18:54:53 -0600

    graficar
    modificaciones menores.

--- arbolavl.h
+++ arbolavl.h
@@ -1,9 +1,5 @@
 #ifndef ARBOLAVL_H
 #define ARBOLAVL_H
-#include "stdlib.h"
-#include "stdio.h"
-#include "string.h"
-#include "iostream"
 
 typedef struct NodoAVL NodoAVL;
 typedef struct ArbolAVL ArbolAVL;
